0d049b866496e6810e33dd379251d62c072b125f
Andre Perkins <akperkins1@gmail.com>
2015-03-31 23:29:33 -0400

    added the fail hard test case

--- src/com/overnightapps/NullObjectGenerator.java
+++ src/com/overnightapps/NullObjectGenerator.java
@@ -3,22 +3,51 @@ package com.overnightapps;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.FixedValue;
 
-import java.util.Objects;
-
 /**
  * Created by andreperkins on 3/31/15.
  */
 public class NullObjectGenerator {
+
+    private final boolean isSetToFailHard;
+
+    public NullObjectGenerator(boolean isSetToFailHard) {
+        this.isSetToFailHard = isSetToFailHard;
+    }
+
+    public NullObjectGenerator() {
+        this.isSetToFailHard = false;
+    }
+
     public <T extends Object> T generate(Class<T> testInterfaceClass) {
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(testInterfaceClass);
         enhancer.setCallback(new FixedValue() {
             @Override
             public Object loadObject() throws Exception {
+                if(isSetToFailHard){
+                    throw new AssertionError("A null object was not expected here.");
+                }
                 return "Hello cglib!";
             }
         });
         T proxy = (T) enhancer.create();
         return proxy;
+    }
+
+    public boolean isSetToFailHard() {
+        return isSetToFailHard;
+    }
+
+    public static class Builder {
+        private boolean isSetToFailHard;
+
+        public Builder setFailHard() {
+            isSetToFailHard = true;
+            return this;
+        }
+
+        public NullObjectGenerator build() {
+            return new NullObjectGenerator(isSetToFailHard);
+        }
     }
 }
--- src/test/NullObjectGeneratorTest.java
+++ src/test/NullObjectGeneratorTest.java
@@ -39,4 +39,19 @@ public class NullObjectGeneratorTest extends TestCase {
         assertNotNull(testInterface);
         testInterface.doNotReturn();
     }
+
+    public void test_generate_passTestClassWithHardFailSet_anExceptionShouldBeThrown(){ //todo replace these junit 3 tests with junit 4
+        nullObjectGenerator = new NullObjectGenerator(true);
+        TestInterface testInterface = nullObjectGenerator.generate(TestClass.class);
+        assertNotNull(testInterface);
+        boolean isExceptionThrown;
+
+        try {
+            testInterface.doNotReturn();
+            isExceptionThrown = false;
+        } catch (AssertionError e){
+            isExceptionThrown = true;
+        }
+        assertTrue(isExceptionThrown);//todo replace with assertj
+    }
 }
